a7ee9394f72abada2164dd9ac087662d591fe3b1
Eyre_S <SteveW2004@outlook.com>
2020-02-23 13:00:04 +0800

    更新版本号（刚刚忘记了=-=

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: SukaBerg 是一个整合了一些 Sukazyo Workshop 中经常会用到的区块的 Gutenberg 扩展包
  * Author: Cookie Sukazyo Eyre
  * Author URI: https://sukazyo.cc/
- * Version: 1.0.1
+ * Version: 1.1.0
  *
  * @package CGB
  */
